cd3c7d8149162acf50fca03be2653eaee07a333f
Maxime TheSpyGeek <isnel.maxime@gmail.com>
2019-12-09 11:58:33 +0100

    fix du bug des rigidbody sur le player close #1

--- GarbageQuest/Assets/Scripts/Controller.cs
+++ GarbageQuest/Assets/Scripts/Controller.cs
@@ -5,23 +5,23 @@ public class Controller : MonoBehaviour {
 
 	public float moveSpeed = 6;
 
-	Rigidbody rigidbody;
+	Rigidbody m_rigidbody;
 	Camera viewCamera;
 	Vector3 velocity;
 
 	void Start () {
-		rigidbody = GetComponent<Rigidbody> ();
+		m_rigidbody = GetComponent<Rigidbody> ();
 		viewCamera = Camera.main;
 	}
 
-	void Update () {
-		Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
-		transform.LookAt (mousePos + Vector3.up * transform.position.y);
-		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * moveSpeed;
-	}
+
 
 	void FixedUpdate() {
-		rigidbody.MovePosition (rigidbody.position + velocity * Time.fixedDeltaTime);
-		transform.position = rigidbody.position;
-	}
+        Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
+        transform.LookAt(mousePos + Vector3.up * transform.position.y);
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+        //m_rigidbody.MovePosition (m_rigidbody.position + velocity * Time.fixedDeltaTime);
+        m_rigidbody.velocity = velocity;
+        //transform.position = rigidbody.position;
+    }
 }
