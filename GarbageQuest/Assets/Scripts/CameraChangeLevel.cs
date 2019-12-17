52095bcccfef55a999375838978750eccaeb788d
Maxime TheSpyGeek <isnel.maxime@gmail.com>
2019-12-17 12:55:54 +0100

    ajout de mouvement de camera quand on passe la porte

---
+++ GarbageQuest/Assets/Scripts/CameraChangeLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChangeLevel : MonoBehaviour
+{
+
+    public Transform m_nextLevelTransform;
+
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player")) {
+            GameObject obj = Camera.main.gameObject;
+            smoothTransformMove smooth = obj.GetComponent<smoothTransformMove>();
+            if(smooth != null) {
+                smooth.endMarker = m_nextLevelTransform;
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+                if(player != null) {
+                    player.transform.position = new Vector3(m_nextLevelTransform.position.x, m_nextLevelTransform.position.y + 0.1f, m_nextLevelTransform.position.z);
+                }
+            }
+        }
+    }
+}
---
+++ GarbageQuest/Assets/Scripts/smoothTransformMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoothTransformMove : MonoBehaviour
+{
+    public Transform endMarker;
+    public float m_speed = 1.2f;
+
+    void Update() {
+
+        transform.position = Vector3.Lerp(transform.position, new Vector3(endMarker.position.x, transform.position.y, endMarker.position.z), Time.deltaTime* m_speed);
+    }
+}
