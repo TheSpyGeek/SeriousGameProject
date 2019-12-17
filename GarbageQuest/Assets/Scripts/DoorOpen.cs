9d3b6142b7f51b91ae9aeca4c5473bd6242b7482
Maxime TheSpyGeek <isnel.maxime@gmail.com>
2019-12-17 12:14:39 +0100

    ouverture des portes quand il n'y a plus d'enemy

---
+++ GarbageQuest/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    
+
+    private Animation m_animation;
+    private bool m_animationStarted;
+
+    private void Start() {
+        m_animationStarted = false;
+        m_animation = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(!m_animationStarted) {
+            if(noEnemies()) {
+                m_animation.Play();
+                m_animationStarted = true;
+                Debug.Log("Start");
+            }
+        }
+    }
+
+    bool noEnemies() {
+        GameObject[] array = GameObject.FindGameObjectsWithTag("Enemy");
+        return array.Length == 0;
+    }
+}
