71ec855df9e8bba4ba92e9c01ebe49c6b787f0bc
NicolasCalvet <47445393+NicolasCalvet@users.noreply.github.com>
2019-12-30 13:56:38 +0100

    Poubelles et Personnage fonctionnent
    Quelques petits bugs de collisions qui font légèrement "slide" le personnage sont encore présents

---
+++ GarbageQuest/Assets/AnimationControllerPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControllerPlayer : MonoBehaviour
+{
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0){
+            animator.SetBool("running", true);
+        }else{
+            animator.SetBool("running", false);
+        }
+
+        if (Input.GetAxis("Attack") != 0 ){
+            animator.SetBool("attacking", true);
+        }else{
+            animator.SetBool("attacking", false);
+        }
+    }
+}
