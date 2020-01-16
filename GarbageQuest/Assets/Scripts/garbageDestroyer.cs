978ee5b2671333f471ab82284c414d20e8da8108
NicolasCalvet <47445393+NicolasCalvet@users.noreply.github.com>
2020-01-16 18:23:33 +0100

    Ajout des poubelles différentes + scoring
    Attention, certains prefab variant sont passés en original prefab

--- GarbageQuest/Assets/Scripts/garbageDestroyer.cs
+++ GarbageQuest/Assets/Scripts/garbageDestroyer.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Type))]
 
 public class garbageDestroyer : MonoBehaviour
 {
 
-    
-
     private Collider _colllider;
+    public Type type;
+    public GameObject gameState;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +20,33 @@ public class garbageDestroyer : MonoBehaviour
 
 
     private void OnTriggerEnter(Collider other) {
+
         if(other.CompareTag("Enemy")) {
+
             Health _heal;
             if(other.gameObject.TryGetComponent<Health>(out _heal)) {
+
                 if(!_heal.isAlive()) {
+
+                    Type _type;
+                    if (other.gameObject.TryGetComponent<Type>(out _type)){
+                        
+                        if (type.m_type == _type.m_type){
+                            // Score s = GameObject.Find("GameState").GetComponent<Score>();
+                            gameState.GetComponent<Score>().addScore(10);
+                        }else{
+                            // Score s = GameObject.Find("GameState").GetComponent<Score>();
+                            gameState.GetComponent<Score>().loseScore(10);
+                        }
+
+                    }
+                    
                     Destroy(other.gameObject);
+
                 }
+
             }
         }
+
     }
 }
