92bf0800489c7bc0b5d509e604e123e37e149ae6
NicolasCalvet <47445393+NicolasCalvet@users.noreply.github.com>
2020-01-16 15:02:17 +0100

    Asset du boss

---
+++ GarbageQuest/Assets/Boss_Life.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Life : MonoBehaviour
+{
+    public Health _health;
+
+    public List<GameObject> childsPrefabs;
+    public float radius = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( ! _health.isAlive()) {
+
+            // Spawning parts independently
+
+            for (int i = 0; i < childsPrefabs.Count; i++)
+            {
+                float angle = i * Mathf.PI * 2 / childsPrefabs.Count;
+
+                float x = Mathf.Cos(angle) * radius;
+                float z = Mathf.Sin(angle) * radius;
+                Vector3 pos = transform.position + new Vector3(x, 0, z);
+                
+                Instantiate(childsPrefabs[i], pos, childsPrefabs[i].transform.rotation);
+            }
+
+            // Lastly destroy itself
+            Destroy(gameObject);
+        }
+    }
+}
