808d0d0b83a22098ea2d57d6df58e40d0c94500a
Maxime TheSpyGeek <isnel.maxime@gmail.com>
2020-01-16 15:08:40 +0100

    changement de la camera pour suivre le personnage

---
+++ GarbageQuest/Assets/Scripts/PlayerVillageWalk.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVillageWalk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator animator = GetComponent<Animator>();
+        animator.SetBool("running", true);
+    }
+
+}
